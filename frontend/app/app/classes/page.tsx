7f3b84f54e98eedfcd0c3a8a9ac5c7617666effd
AungS8430 <aungshome@outlook.com>
2026-02-12 23:28:17 +0700

    Added classes page

---
+++ frontend/app/app/classes/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import {
+  Combobox,
+  ComboboxContent,
+  ComboboxEmpty,
+  ComboboxInput,
+  ComboboxItem,
+  ComboboxList
+} from "@/components/ui/combobox";
+import {
+  Card,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { List, Users } from "lucide-react";
+import Link from "next/link";
+
+export default function ClassesPage() {
+  const [gradeList, setGradeList] = useState<string[]>(["1st Year", "2nd Year", "3rd Year"]);
+  const [selectedGrade, setSelectedGrade] = useState<string>("2nd Year");
+  const [classList, setClassList] = useState<string[]>([
+    "C2R1",
+    "C2R2",
+    "M2R1",
+    "M2R2",
+    "E2R1",
+    "E2R2"
+  ]);
+
+  useEffect(() => {
+    if (!selectedGrade && gradeList.length > 0) {
+      setSelectedGrade(classList[0]);
+    }
+  }, [selectedGrade, gradeList]);
+
+  const handleGradeChange = (value: string | null) => {
+    if (value) {
+      setSelectedGrade(value);
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-start items-center max-w-5xl w-full h-screen gap-0 mx-auto overflow-hidden">
+      <div className="flex flex-col gap-2 sticky top-0 z-10 w-full p-3">
+        <h1 className="text-2xl font-semibold">Classes</h1>
+        <div className="flex gap-2 items-center w-full max-w-full">
+          <Combobox required items={gradeList} value={selectedGrade} onValueChange={handleGradeChange} autoHighlight>
+            <ComboboxInput placeholder="Select a class..." showClear={false} className="grow" />
+            <ComboboxContent>
+              <ComboboxEmpty>No class found.</ComboboxEmpty>
+              <ComboboxList>
+                {(item) => (
+                  <ComboboxItem key={item} value={item}>
+                    {item}
+                  </ComboboxItem>
+                )}
+              </ComboboxList>
+            </ComboboxContent>
+          </Combobox>
+        </div>
+      </div>
+      <div className="p-4 w-full overflow-x-auto max-w-full mb-12 md:mb-0 flex flex-col gap-2">
+        {
+          classList.map((className) => (
+            <Card key={className} size="sm" className="bg-card/50">
+              <CardHeader>
+                <CardTitle>{className}</CardTitle>
+              </CardHeader>
+              <Separator />
+              <CardFooter>
+                <Button variant="link" size="sm" asChild>
+                  <Link href={`/app/schedule?class=${className}`}><List /> View Schedule</Link>
+                </Button>
+                <Button variant="link" size="sm" asChild>
+                  <Link href={`/app/students?class=${className}`}><Users /> View People</Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
