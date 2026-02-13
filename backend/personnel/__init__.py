4a35f43e6ffa7dc74b240cda402e88aa743481e0
Ryunaja <Kawiwat.ryu@gmail.com>
2026-02-13 14:43:40 +0700

    update some functions header

--- backend/personnel/__init__.py
+++ backend/personnel/__init__.py
@@ -1,1 +1,8 @@
+from personnel.personnel import (
+    check_permission_to_edit,  # noqa : F401
+    edit_personnel,  # noqa : F401
+    fetch_all_personnel,  # noqa : F401
+    fetch_by_tags,  # noqa : F401
+    fetch_personnel_by_personnelID,  # noqa : F401
+)
 from personnel.personnelModel import Personnel, RoleEnum  # noqa : F401
