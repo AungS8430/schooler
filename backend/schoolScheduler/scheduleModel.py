d02eddc9403db5fcd917964a2f53d703b83b8ee1
Ryunaja <Kawiwat.ryu@gmail.com>
2026-02-13 11:01:17 +0700

    add descriptions and type of override schedule

--- backend/schoolScheduler/scheduleModel.py
+++ backend/schoolScheduler/scheduleModel.py
@@ -1,6 +1,16 @@
 from dataclasses import dataclass
+from enum import Enum
 
 TIME_LOOKUP = {1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday"}
+
+
+class OverrideType(Enum):
+    CLASS = "class"
+    HOLIDAY = "holiday"
+    EXAM = "exam"
+    EVENT = "event"
+    OTHER = "other"
+    BREAK = "break"
 
 
 @dataclass
