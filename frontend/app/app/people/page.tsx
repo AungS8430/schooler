90e65f635ab3a3fe72ab656be6799af1cd2920f4
AungS8430 <aungshome@outlook.com>
2026-02-14 14:06:48 +0700

    Added dialogs to people page and improved it overall

--- frontend/app/app/people/page.tsx
+++ frontend/app/app/people/page.tsx
@@ -9,17 +9,27 @@ import {
   ComboboxItem,
   ComboboxList
 } from "@/components/ui/combobox";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ButtonGroup } from "@/components/ui/button-group";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { Mail, Phone } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Mail, Phone, Activity, Users, School } from "lucide-react";
 import Link from "next/link";
 
 interface Person {
   id: number;
   firstName: string;
   lastName: string;
+  profilePicture?: string;
   middleName?: string;
   nickname?: string;
   class?: string;
@@ -216,8 +226,8 @@ export default function ClassesPage() {
         <h1 className="text-2xl font-semibold">People</h1>
         <div className="flex gap-2 items-center w-full max-w-full">
           <Input placeholder="Search people..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className="grow" />
-          <Combobox items={classList} value={selectedClass} onValueChange={(value) => setSelectedClass(value)} autoHighlight>
-            <ComboboxInput placeholder="Select a class..." showClear={false} />
+          <Combobox items={classList} value={selectedClass || ""} onValueChange={(value) => setSelectedClass(value)} autoHighlight>
+            <ComboboxInput placeholder="Select a class..." showClear={true} />
             <ComboboxContent>
               <ComboboxEmpty>No class found.</ComboboxEmpty>
               <ComboboxList>
@@ -229,8 +239,8 @@ export default function ClassesPage() {
               </ComboboxList>
             </ComboboxContent>
           </Combobox>
-          <Combobox items={gradeList} value={selectedGrade} onValueChange={(value) => setSelectedGrade(value)} autoHighlight>
-            <ComboboxInput placeholder="Select a grade..." showClear={false} />
+          <Combobox items={gradeList} value={selectedGrade || ""} onValueChange={(value) => setSelectedGrade(value)} autoHighlight>
+            <ComboboxInput placeholder="Select a grade..." showClear={true} />
             <ComboboxContent>
               <ComboboxEmpty>No class found.</ComboboxEmpty>
               <ComboboxList>
@@ -248,34 +258,108 @@ export default function ClassesPage() {
         <div className="flex flex-col gap-2 mb-12 md:mb-0">
           {
             people.map((person) => (
-              <div key={person.id} className="bg-card/50 p-4 rounded-4xl shadow-md border">
-                <div className="flex flex-col sm:flex-row gap-2">
-                  <div>
-                    <div className="my-auto font-semibold">
-                      {person.firstName} {person.lastName} <span className="text-muted-foreground">{person.nickname}</span>
-                    </div>
-                    <div className="font-normal text-sm my-auto">
-                      {person.role} | {person.department} Department {person.class && `| ${person.class}`}
+              <Dialog key={person.id}>
+                <DialogTrigger asChild>
+                  <div className="bg-card/50 p-4 rounded-4xl shadow-md border hover:cursor-pointer hover:underline">
+                    <div className="flex flex-col sm:flex-row gap-2">
+                      <div className="flex flex-row gap-4">
+                        <Avatar className="my-auto">
+                          {person.profilePicture ? (
+                            <AvatarImage src={person.profilePicture} alt={`${person.firstName} ${person.lastName}'s profile picture`} />
+                          ) : (
+                            <AvatarFallback>{person.firstName.charAt(0)}{person.lastName.charAt(0)}</AvatarFallback>
+                          )}
+                        </Avatar>
+                        <div>
+                          <div className="my-auto font-semibold">
+                            {person.firstName} {person.lastName} <span className="text-muted-foreground">{person.nickname}</span>
+                          </div>
+                          <div className="font-normal text-sm my-auto">
+                            {person.role} | {person.department} Department {person.class && `| ${person.class}`}
+                          </div>
+                        </div>
+                      </div>
+                      <div onClick={(e) => e.stopPropagation()} className="w-full sm:w-auto ml-auto my-auto">
+                        <ButtonGroup className="w-full sm:w-auto ml-auto my-auto">
+                          <Button variant="outline" size="icon" asChild={!!person.phone} disabled={!person.phone} className="grow sm:grow-0">
+                            {person.phone ? (
+                              <Link href={`tel:${person.phone}`}><Phone /></Link>
+                            ) : (
+                              <Phone />
+                            )}
+                          </Button>
+                          <Button size="icon" asChild={!!person.email} disabled={!person.email} className="grow sm:grow-0">
+                            {person.email ? (
+                              <Link href={`mailto:${person.email}`}><Mail /></Link>
+                            ) : (
+                              <Mail />
+                            )}
+                          </Button>
+                        </ButtonGroup>
+                      </div>
                     </div>
                   </div>
-                  <ButtonGroup className="w-full sm:w-auto ml-auto my-auto">
-                    <Button variant="outline" size="icon" asChild={!!person.phone} disabled={!person.phone} className="grow sm:grow-0">
-                      {person.phone ? (
-                        <Link href={`tel:${person.phone}`}><Phone /></Link>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader className="flex flex-row gap-4">
+                    <Avatar className="my-auto">
+                      {person.profilePicture ? (
+                        <AvatarImage src={person.profilePicture} alt={`${person.firstName} ${person.lastName}'s profile picture`} />
                       ) : (
-                        <Phone />
+                        <AvatarFallback>{person.firstName.charAt(0)}{person.lastName.charAt(0)}</AvatarFallback>
                       )}
-                    </Button>
-                    <Button size="icon" asChild={!!person.email} disabled={!person.email} className="grow sm:grow-0">
-                      {person.email ? (
-                        <Link href={`mailto:${person.email}`}><Mail /></Link>
-                      ) : (
-                        <Mail />
-                      )}
-                    </Button>
-                  </ButtonGroup>
-                </div>
-              </div>
+                    </Avatar>
+                    <div className="my-auto flex flex-col justify-center">
+                      <DialogTitle>{person.firstName} {person.lastName}</DialogTitle>
+                      <DialogDescription>{person.nickname}</DialogDescription>
+                    </div>
+                  </DialogHeader>
+
+                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
+                    <div className="flex flex-row gap-2 items-center">
+                      <Activity className="size-4" />
+                      <div className="flex flex-col">
+                        <div className="text-muted-foreground text-xs font-semibold">Role</div>
+                        <div>{person.role}</div>
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-2 items-center">
+                      <Users className="size-4" />
+                      <div className="flex flex-col">
+                        <div className="text-muted-foreground text-xs font-semibold">Department</div>
+                        <div>{person.department}</div>
+                      </div>
+                    </div>
+                    {person.class && (
+                      <div className="flex flex-row gap-2 items-center mt-4">
+                        <School className="size-4" />
+                        <div className="flex flex-col">
+                          <div className="text-muted-foreground text-xs font-semibold">Class</div>
+                          <div>{person.class}</div>
+                        </div>
+                      </div>
+                    )}
+                    {person.email && (
+                      <div className="flex flex-row gap-2 items-center mt-4">
+                        <Mail className="size-4" />
+                        <div className="flex flex-col">
+                          <div className="text-muted-foreground text-xs font-semibold">Email</div>
+                          <div>{person.email}</div>
+                        </div>
+                      </div>
+                    )}
+                    {person.phone && (
+                      <div className="flex flex-row gap-2 items-center mt-4">
+                        <Phone className="size-4" />
+                        <div className="flex flex-col">
+                          <div className="text-muted-foreground text-xs font-semibold">Phone</div>
+                          <div>{person.phone}</div>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </DialogContent>
+              </Dialog>
             ))
           }
         </div>
