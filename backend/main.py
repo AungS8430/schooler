94922b086e212e7d1f5cab26f0a56cabb8569d02
Ryunaja <Kawiwat.ryu@gmail.com>
2026-02-17 09:52:30 +0700

    add dummy resources endpoint

--- backend/main.py
+++ backend/main.py
@@ -393,3 +393,14 @@ def get_people(
     users = session.exec(exected_query).all()
     users = [x.model_dump_json() for x in users]
     return {"users": users}
+
+
+@app.get("/resources")
+def get_resources():
+    return {
+        "id": 1,
+        "title": "Example Resource",
+        "author": "Jeffery Doe",
+        "url": "https://drive.google.com/drive/u",
+        "categorites": ["default", "default"],
+    }
