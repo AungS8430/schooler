9ae2676bbf1e8eda0179840ed2bb42b85acf9e5b
Ryunaja <Kawiwat.ryu@gmail.com>
2026-02-17 23:35:53 +0700

    fix keyerror

--- backend/schoolScheduler/academicYear.py
+++ backend/schoolScheduler/academicYear.py
@@ -17,7 +17,7 @@ def get_events(room: Room) -> list[Event]:
                 id=idx,
                 date=event["date"],
                 duration=event.get("duration", 1),
-                title=event["title"],
+                title=event["event"],
                 description=event.get("description", ""),
                 type=event.get("type", "general"),
             )
@@ -34,7 +34,7 @@ def get_events_all() -> list[Event]:
                 id=idx,
                 date=event["date"],
                 duration=event.get("duration", 1),
-                title=event["title"],
+                title=event["event"],
                 description=event.get("description", ""),
                 type=event.get("type", "general"),
             )
