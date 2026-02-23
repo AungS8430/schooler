6409cb7adbd8d0fa60c2211627aacb68acd3d12b
AungS8430 <aungshome@outlook.com>
2026-02-23 10:14:46 +0700

    Auto set the class and departments for the demo

--- backend/app/schemas/types.py
+++ backend/app/schemas/types.py
@@ -22,16 +22,14 @@ class RoleEnum(str, Enum):
 
 
 TIME_LOOKUP = {1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday"}
-GRADE = ["year1", "year2", "year3", "year4", "year5"]
-DEPARTMENT = ["Computer Engineering", "Mechatronics Engineering", "Electrical and Electronics Engineering"]
+GRADE = ["year2"]
+DEPARTMENT = ["Computer Engineering"]
 ROOM = [f"room{x}" for x in range(1, 11)]
 GRADE_LOOKUP = {
-    1: "1st Year",
     2: "2nd Year",
-    3: "3rd Year",
 }
 CLASSES_LOOKUP = {
-    2: {"Computer Engineering": ["C2R1", "C2R2"], "Mechatronics Engineering": ["M2R1", "M2R2"], "Electrical and Electronics Engineering": ["E2R1", "E2R2"]},
+    2: {"Computer Engineering": ["C2R1", "C2R2"]},
 }
 
 
